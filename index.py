289a281b3fcb7f3427b04887a0ccc9ccbe33d3c3
Hemashree-Jawdekar <168700546+Hemashree-Jawdekar@users.noreply.github.com>
2026-05-10 10:50:44 +0530

    Enhance hello function with day greetings
    Added day-specific greetings in French for each weekday.

--- index.py
+++ index.py
@@ -1,6 +1,22 @@
 def hello():
   print("Hello One and All!\n")
-  day = input("What is Today's Day?");
+  day = input("What is Today's Day?")
   print("It is a Beautiful ", day)
+  if(day.lower() == "monday"):
+    print("il fait beau lundi")
+  elif day.lower() == "tuesday":
+    print("il fait beau mardi")
+  elif day.lower() == "wednesday":
+    print("il fait beau mercredi")
+  elif day.lower() == "thursday":
+    print("il fait beau jeudi")
+  elif day.lower() == "friday":
+    print("il fait beau vendredi")
+  elif day.lower() == "saturday":
+    print("il fait beau samedi")
+  elif day.lower() == "sunday":
+    print("il fait beau dimanche")
+  else:
+    print("bebe! ce n'est pas un jour de la semaine")
 
 hello()
